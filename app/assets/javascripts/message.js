103c6dac77e6816b7f5cc6ee9f476029b64830b1
kenchanbass <ken.inu.who@gmail.com>
2020-04-01 20:07:30 +0900

    自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){ 
+
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="messages__message">
+      `<div class="messages__message" data-message-id=${message.id}>
          <div class="messages__message__info">
            <div class="messages__message__info__talker">
              ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="messages__message">
+      `<div class="messages__message" data-message-id=${message.id}>
          <div class="messages__message__info">
            <div class="messages__message__info__talker">
              ${message.user_name}
@@ -62,4 +63,31 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.messages__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
